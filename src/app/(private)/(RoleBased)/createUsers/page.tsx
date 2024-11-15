8c61a173ecf149234f91b238aa24dcd6e1b48698
Ambuj Singh <ambujsingh@Ambujs-MacBook-Air.local>
2024-11-15 16:02:10 +0530

    changes

--- src/app/(private)/(RoleBased)/createUsers/page.tsx
+++ src/app/(private)/(RoleBased)/createUsers/page.tsx
@@ -1,20 +1,26 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Input } from "@nextui-org/react";
-import Link from "next/link";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
-import { errorToast, successToast } from "../../../utility/toast";
+import { errorToast, successToast } from "@/utility/toast";
 import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
 
-// Generate a unique ID for each meeting room (you can use libraries like uuid for this)
-const generateId = () => `room_${Math.random().toString(36).substr(2, 9)}`;
-
-const SignUp = () => {
+const CreateUser = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
+  const [userData, setUserData] = useState<any>(null);
+
+  useEffect(() => {
+    const userInfo = Cookies.get("userInfo")
+      ? JSON.parse(Cookies.get("userInfo") || "")
+      : null;
+    setUserData(userInfo);
+  }, []);
+
   // Yup validation schema
   const validationSchema = Yup.object({
     name: Yup.string().required("Name is required"),
@@ -24,18 +30,6 @@ const SignUp = () => {
     password: Yup.string()
       .min(8, "Password must be at least 8 characters")
       .required("Password is required"),
-    company: Yup.string().required("Company name is required"),
-    numberofmeetingroom: Yup.number()
-      .required("Number of meeting rooms is required")
-      .min(1, "Number of meeting rooms should be at least 1")
-      .max(10, "Number of meeting rooms should be less than or equal to 10"),
-    meetingRooms: Yup.array()
-      .of(
-        Yup.object().shape({
-          name: Yup.string().required("Meeting room name is required"),
-        })
-      )
-      .min(1, "At least 1 meeting room is required"),
   });
 
   // Initial form values
@@ -43,15 +37,14 @@ const SignUp = () => {
     name: "",
     email: "",
     password: "",
-    company: "",
-    role: "admin",
-    numberofmeetingroom: "", // Default to 1 meeting room
-    meetingRooms: [], // This will store an array of objects with id and name
+    role: "user",
+    company: userData?.companyId || "", // set company dynamically based on userData
   };
 
   // Form submit handler
   const onSubmit = (values: any, { resetForm }: { resetForm: () => void }) => {
     setIsLoading(true);
+    console.log(values, "values============");
     // Handle form submission here
     const sendToDatabase = async () => {
       try {
@@ -83,7 +76,6 @@ const SignUp = () => {
     };
 
     sendToDatabase();
-    //reset the form
     resetForm();
   };
 
@@ -94,10 +86,11 @@ const SignUp = () => {
           initialValues={initialValues}
           validationSchema={validationSchema}
           onSubmit={onSubmit}
+          enableReinitialize // reinitialize form values when initialValues changes
         >
-          {({ values, setFieldValue, errors, touched }) => (
+          {({ errors, touched }) => (
             <Form className="flex flex-col gap-7 p-10 border border-gray-300 rounded-lg shadow-lg lg:w-1/3 md:w-1/2 w-full">
-              <h1 className="text-3xl font-bold text-primary">Sign Up</h1>
+              <h1 className="text-3xl font-bold text-primary">Create User</h1>
 
               <div className="flex flex-col">
                 <Field
@@ -157,81 +150,6 @@ const SignUp = () => {
                 />
               </div>
 
-              <div className="flex flex-col">
-                <Field
-                  name="company"
-                  as={Input}
-                  type="text"
-                  label="Company Name"
-                  placeholder="Enter your company name"
-                  error={errors.company && touched.company}
-                />
-                <ErrorMessage
-                  name="company"
-                  component="div"
-                  className="text-red-500 text-sm mt-1 mx-2 min-h-5"
-                />
-              </div>
-
-              <div className="flex flex-col">
-                <Field
-                  name="numberofmeetingroom"
-                  as={Input}
-                  type="number"
-                  label="Number of Meeting Rooms"
-                  placeholder="Number of Meeting Rooms"
-                  onChange={(e: any) => {
-                    const numRooms = parseInt(e.target.value) || 0;
-                    setFieldValue("numberofmeetingroom", numRooms);
-
-                    // Generate an array of objects with id and empty name
-                    setFieldValue(
-                      "meetingRooms",
-                      Array.from({ length: numRooms }, () => ({
-                        // id: generateId(),
-                        name: "",
-                      }))
-                    );
-                  }}
-                  error={
-                    errors.numberofmeetingroom && touched.numberofmeetingroom
-                  }
-                />
-                <ErrorMessage
-                  name="numberofmeetingroom"
-                  component="div"
-                  className="text-red-500 text-sm mt-1 mx-2 min-h-5"
-                />
-              </div>
-
-              {/* Dynamic inputs for meeting rooms */}
-              {values.meetingRooms.length > 0 &&
-                values.meetingRooms.length < 11 && (
-                  <div>
-                    <h3 className="text-lg font-semibold mt-4">
-                      Meeting Rooms:
-                    </h3>
-                    {values.meetingRooms.map((room: any, index) => (
-                      <div key={room.id} className="flex flex-col mt-2">
-                        <Field
-                          name={`meetingRooms[${index}].name`}
-                          as={Input}
-                          type="text"
-                          label={`Meeting Room ${index + 1} Name`}
-                          placeholder={`Enter name for meeting room ${
-                            index + 1
-                          }`}
-                        />
-                        <ErrorMessage
-                          name={`meetingRooms[${index}].name`}
-                          component="div"
-                          className="text-red-500 text-sm mt-1 mx-2 min-h-5"
-                        />
-                      </div>
-                    ))}
-                  </div>
-                )}
-
               <Button
                 type="submit"
                 color="primary"
@@ -240,15 +158,6 @@ const SignUp = () => {
               >
                 Sign Up
               </Button>
-              <p>
-                Already have an account?{" "}
-                <Link
-                  className="text-primary cursor-pointer font-medium"
-                  href="/login"
-                >
-                  Login
-                </Link>
-              </p>
             </Form>
           )}
         </Formik>
@@ -257,4 +166,4 @@ const SignUp = () => {
   );
 };
 
-export default SignUp;
+export default CreateUser;
