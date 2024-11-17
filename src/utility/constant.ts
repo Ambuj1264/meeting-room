82b430ab7951f08904459d7d46a6c8c9b643dfb0
ambuj1264 <ambuj1264675@gmail.com>
2024-11-17 16:31:29 +0530

    chagnes '

--- src/utility/constant.ts
+++ src/utility/constant.ts
@@ -1,4 +1,5 @@
 export const BRAND_NAME = "Meeting Room";
 export const HERO_HEADING = "Book your next Meeting";
-export const HERO_DESCRIPTION = "Schedule your next meeting with ease. Our team of experts will help you create a professional and engaging meeting."
-export const HERO_ACTION_BUTTON = "Get Started";
+export const HERO_DESCRIPTION =
+  "Schedule your next meeting with ease. Our team of experts will help you create a professional and engaging meeting.";
+export const HERO_ACTION_BUTTON = "Book Now";
